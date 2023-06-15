885d432e9abd76f5fd846412b726de919dd90357
MC874 <mc874.mcpe@gmail.com>
2023-06-15 08:17:32 +0700

    main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import re
 import os
+import re
 import json
 import datetime
 from jsonmerge import merge
+
+location = f'./foodie3.json'
+export = f'{str(datetime.datetime.now().strftime("%Y%m%d"))}.json'
 
 class colors:
 	RED_BG = '\033[41m\033[1m'
@@ -11,45 +14,44 @@ class colors:
 	GREEN = '\033[1;92m'
 	ENDC = '\033[m'
 
-Pricy = []
-Foody = []
-location = f'./{str(datetime.datetime.now().strftime("%Y%m%d"))}.json'
-
-allin = 0
-num_file = 1
-
-print('')
-with open('foodie3.json') as f:
-	data = json.load(f)
-	for x, y in data.items():
-		print(x)
-		for key, value in data[x].items():
-			Foody.append(key)
-			Pricy.append(value)
-			decim = value / 1000
-			print(str(num_file) + ') ' + key + ': [' + colors.PURPLE_BG  + f' {value} ' + colors.ENDC + '] / [' + colors.PURPLE_BG + f' {decim} ' + colors.ENDC + ']')
-			num_file = num_file + 1
+def menu():
+	line = 1
+	total_price = 0
+	hold_price = {}
+	hold_item = {}
+	selected_item = []
+	with open(location, 'r') as file:
+		json_content = json.load(file)
+	for key, value in json_content.items():
+		print(key)
+		for key, value in value.items():
+			print(f'{line}). {key}: [{colors.PURPLE_BG} {value} {colors.ENDC}]')
+			hold_price[line] = value
+			hold_item[line] = key
+			line += 1
 		print('')
-ask = input(' Pilih Menu: ')
-result = [str(x) for x in re.split(', |,| , | ,|; |;| ; | ;', ask)]
-for i in result:
-	if re.search('\*', i):
-		result = [int(x) for x in re.split('\*| \*| \* |\* ', i)]
-		allin = allin + Pricy[result[0]-1] * result[1]
-	else:
-		allin = allin + Pricy[int(i)-1]
-print('Total Harga: ' + colors.GREEN_BG + f' {str(allin)} ' + colors.ENDC )
-print()
-umoney = input(' Uang Pelanggan: ')
-if int(umoney) < 999:
-	kembalian = int(umoney) * 1000 - int(allin)
-else:
-	kembalian = int(umoney) - int(allin)
-kembali = int(kembalian) / 1000
-print('Kembalian: '+ colors.RED_BG + f' {str(kembalian)} ' + colors.ENDC + ' / ' + colors.RED_BG + f' {str(kembali)} ' + colors.ENDC)
+	selected_menu = [int(x) for x in re.split(', |,| , | ,|; |;| ; | ;', input('Choose Menu: '))]
+	for selection in selected_menu:
+		total_price += hold_price[selection]
+		item_name = hold_item[selection]
+		if ']' in item_name:
+			selected_item.append(re.search('\]\ ([a-zA-Z\ \&]*)$', item_name).group(1))
+		else:
+			selected_item.append(item_name)
+	print(f'Total Price: {colors.GREEN_BG} {total_price} {colors.ENDC}')
+	customer_price = int(input('Customer Price: ')) * 1000
+	kembalian = customer_price - total_price
+	print(f'Paybacks: {colors.RED_BG} {customer_price - total_price} {colors.ENDC}')
+	profits = customer_price - kembalian
+	print(f'Profits: {colors.GREEN_BG} {profits} {colors.ENDC}')
+	print('')
+	with open(export, 'r+') as file:
+		json_content = json.load(file)
+		file.seek(0)
+		file.write(json.dumps(merge(json_content, {str(datetime.datetime.now().strftime('%Y%m%d_%H%M%S')):{'received': customer_price, 'given': kembalian, 'profits': profits, 'products': selected_item}}), sort_keys = True, indent = 4))
 
-if os.path.isfile(location) is False:
-	with open(location, 'w') as f:
-		f.write('{}')
-with open(location, 'r+') as f:
-	json_content = json.load(f)
+if os.path.isfile(export) is False:
+	with open(export, 'w') as file:
+		file.write('{}')
+while True:	
+	menu()
